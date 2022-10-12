2b4ea379d8ebcbca43eeadfc851d67d7f78e16e9
hotsause0 <91559201+hotsause0@users.noreply.github.com>
2022-10-12 18:53:30 +0200

    Add project files.

---
+++ powtorka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powtorka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sloik1 = new Sloik("Walec", "Biały", 4.5, true);
+
+
+
+            Console.WriteLine(sloik1.ToString();
+
+
+           Console.ReadLine()
+        }
+    }
+}
---
+++ powtorka/Sloik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powtorka
+{
+    class Sloik
+    {
+
+        public string Kształt { get; set; }
+
+        public string Kolor { get; set; }
+
+        public double Srednica { get; set; }
+
+        public bool CzySzklany { get; set; }
+
+
+
+        public Sloik(string ksztalt, string kolor, double srednica, bool czyszklany)
+        {
+            Kształt = ksztalt;
+            Kolor = kolor;
+            Srednica = srednica;
+            CzySzklany = czyszklany;
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            return $"Kolor = {Kolor}, Srednica = {Srednica}, Ksztalt = {Kształt}, Czy szklany = {CzySzklany}";
+        }
+
+
+
+    }
+}
